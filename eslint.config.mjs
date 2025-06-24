153dd3f7f25ffa97cbf40f24ccd6c046b917dbf4
o-k-harmash <o.k.harmash@gmail.com>
2025-06-24 07:11:21 +0300

    add html, css eslint support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,18 @@ import eslintConfigPrettier from "eslint-config-prettier/flat";
 export default antfu(
   {
     stylistic: true,
+    formatters: {
+      /**
+       * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
+       * By default uses Prettier
+       */
+      css: true,
+      /**
+       * Format HTML files
+       * By default uses Prettier
+       */
+      html: true,
+    },
   },
   eslintConfigPrettier,
 );
